00c742d407fed6b4bc1fa601411554c4a65585e1
woo-joo <kimuj0103@gmail.com>
2020-12-06 03:07:56 +0900

    Add swap in and out feature

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -25,3 +25,46 @@ void swap_init(void)
     swap_table = bitmap_create(swap_table_size);
     lock_init(&swap_table_lock);
 }
+
+/* Copies the frame associated with SWAP_IDX from the swap block. */
+void swap_in(size_t swap_idx, void *kpage)
+{
+    size_t i;
+
+    ASSERT(swap_idx < swap_table_size);
+    ASSERT(is_kernel_vaddr(kpage));
+
+    lock_acquire(&swap_table_lock);
+
+    for (i = 0; i < NUM_SECTORS_PER_PAGE; i++)
+        block_read(swap_block, swap_idx * NUM_SECTORS_PER_PAGE + i,
+                   kpage + i * BLOCK_SECTOR_SIZE);
+
+    ASSERT(bitmap_test(swap_table, swap_idx));
+    bitmap_set(swap_table, swap_idx, false);
+
+    lock_release(&swap_table_lock);
+}
+
+/* Copies the frame associated with KPAGE into the swap block. */
+size_t swap_out(void *kpage)
+{
+    size_t swap_idx;
+    size_t i;
+
+    ASSERT(is_kernel_vaddr(kpage));
+
+    lock_acquire(&swap_table_lock);
+
+    swap_idx = bitmap_scan_and_flip(swap_table, 0, 1, false);
+
+    ASSERT(swap_idx < swap_table_size);
+
+    for (i = 0; i < NUM_SECTORS_PER_PAGE; i++)
+        block_write(swap_block, swap_idx * NUM_SECTORS_PER_PAGE + i,
+                    kpage + i * BLOCK_SECTOR_SIZE);
+
+    lock_release(&swap_table_lock);
+
+    return swap_idx;
+}
